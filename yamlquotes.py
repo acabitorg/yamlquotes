a84b83bb5c1cb25ab6ddb3636f18e275cbf1b34b
Acabit Org <acabit.org@gmail.com>
2022-01-09 11:01:53 -0600

    wip

--- yamlquotes.py
+++ yamlquotes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# Copyright 2021 Acabit.org <acabit.org>
+# Copyright 2021 Acabit.org <https://github.com/acabitorg/yamlquotes>
 # License: GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
 
 import argparse
