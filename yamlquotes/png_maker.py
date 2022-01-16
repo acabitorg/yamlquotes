a6b717341e13318c1000b9641903030fa2cfbdce
Acabit Org <acabit.org@gmail.com>
2022-01-16 17:03:31 -0600

    wip

--- yamlquotes/png_maker.py
+++ yamlquotes/png_maker.py
@@ -6,65 +6,12 @@
 import logging
 import os
 
-from .constants import (EM_DASH, OUTDIR)
-from .helpers import (format_text, get_defaults, get_lang)
+from .constants import OUTDIR
+from .helpers import get_defaults
 from .image_utils import ImageText
+from .repr_quote_plaintext import repr_quote_plaintext
 
 logger = logging.getLogger(__name__)
-
-def get_t(qt, defaults):
-    txt = ''
-    txt += format_text(qt['t'], get_lang(qt, defaults))
-    txt += '\n'
-    return txt 
-
-def get_txr(qt, defaults):
-    txt = ''
-    if 'txr' in qt:
-        #translation quotation marks always conform to default language
-        txt += format_text(qt['txr'], defaults['l'])
-        txt += '\n'
-    return txt
-
-def get_a(qt, defaults):
-    return EM_DASH + qt['a']
-
-def get_ac(qt, defaults):
-    txt = ''
-    if 'ac' in qt:
-        txt += ', {}'.format(qt['ac'].rstrip())
-    return txt
-
-def get_d(qt, defaults):
-    txt = ''
-    if 'd' in qt:
-        field_d = str(qt['d'])
-        txt += ', ' + field_d
-    return txt
-
-def get_c(qt, defaults):
-    txt = ''
-    if 'c' in qt:
-        txt += ', {}'.format(qt['c'].rstrip())
-    return txt
-
-def get_g(qt, defaults):
-    txt = ''
-    if 'g' in qt:
-        field_g = qt['g']
-        txt += ', ' + field_g
-    return txt
-
-def repr_quote_plaintext(qt, defaults):
-    txt = ''
-    txt += get_t(qt, defaults)
-    txt += get_txr(qt, defaults)
-    txt += get_a(qt, defaults)
-    txt += get_ac(qt, defaults)
-    txt += get_c(qt, defaults)
-    txt += get_d(qt, defaults)
-    txt += get_g(qt, defaults)
-    return txt
 
 FONT_SCALE_FRAME_MULTIPLIER = 2 # multiplies the effect of the font-scaling
 # on the video duration (smaller font-size = more video frames = longer reading time)
