c2f5500d81fe1afb3377e820e0a318b9174a72a4
Acabit Org <acabit.org@gmail.com>
2022-01-16 17:37:42 -0600

    wip

---
+++ scripts/twitter_bot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import time
+from twython import Twython, TwythonError
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+screen_name = 'acabit.org'
+
+from yamlquotes import (load_quotes, repr_quote_plaintext)
+
+data = load_quotes.load_quotes('../yamlquotes/data/quotes.yml')
+
+
+#for i, q in enumerate(data['quotes']):
+#    print('{} {}'.format(i, q['t']))
+
+try:
+    twitter.update_status(status='Test 42')
+except TwythonError as e:
+    print(e)
