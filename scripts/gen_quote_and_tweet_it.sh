43865e4429db1bbbf617d2b0b88a2499760dcb45
Acabit Org <acabit.org@gmail.com>
2022-01-17 21:15:26 -0600

    wip

--- scripts/gen_quote_and_tweet_it.sh
+++ scripts/gen_quote_and_tweet_it.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-cd $YAMLQUOTES_HOME
-python3 -m yamlquotes -f yamlquotes/data/quotes.yml --sort-randomly --print --max 1 > ./scripts/twitter_bot_input.txt
-cd scripts
-DISPLAY=:0 python3 twitter_bot_selenium.py twitter_bot_input.txt
+while :
+do
+	cd ..
+	python3 -m yamlquotes -f yamlquotes/data/quotes.yml --sort-randomly --print --max 1 > ./scripts/twitter_bot_input.txt
+	cd scripts
+	DISPLAY=:0 python3 twitter_bot_selenium.py twitter_bot_input.txt
+	sleep 3600
+done
