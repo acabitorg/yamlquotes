082b1add09d7b45c32e52b8d9d1277fabce417fd
acabitorg <acabitorg@gmail.com>
2022-07-10 19:29:46 -0500

    wip

--- yamlquotes/args_apply_util.py
+++ yamlquotes/args_apply_util.py
@@ -21,11 +21,29 @@ class UtilArgsApplier():
         self.quotes_s = []
         self.device = torch.device('cuda')
         torch.set_num_threads(4)
-        local_file = 'model-en.pt'
-        if not os.path.isfile(local_file):
-            torch.hub.download_url_to_file('https://models.silero.ai/models/tts/en/v3_en.pt', local_file)
-        self.model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
-        self.model.to(self.device)
+        self.models = {}
+        lang_model_urls = {
+            'eng': 'https://models.silero.ai/models/tts/en/v3_en.pt',
+            'deu': 'https://models.silero.ai/models/tts/de/v3_de.pt',
+            'fra': 'https://models.silero.ai/models/tts/fr/v3_fr.pt',
+            'spa': 'https://models.silero.ai/models/tts/es/v3_es.pt',
+            'rus': 'https://models.silero.ai/models/tts/ru/v3_1_ru.pt'
+        }
+        self.lang_speakers = {
+            'eng': 'en_0',
+            'deu': 'eva_k',
+            'fra': 'fr_0',
+            'spa': 'es_0',
+            'rus': 'aidar',
+        }
+        self.random_speaker = True # overrides self.lang_speakers
+
+        for lang, url in lang_model_urls.items():
+            local_file = f'model-{lang}.pt'
+            if not os.path.isfile(local_file):
+                torch.hub.download_url_to_file(url, local_file)
+            self.models[lang] = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+            self.models[lang].to(self.device)
 
     def apply(self, data):
         self.defaults = get_defaults(data)
@@ -62,26 +80,56 @@ class UtilArgsApplier():
             print(s)
 
     def __speak_quotes(self, outfmt=None):
-        for i, s in enumerate(self.quotes_s):
-            self.__speak_quote(i, s, outfmt)
+        for i, qt in enumerate(self.quotes):
+            self.__speak_quote(i, qt, outfmt)
 
-    def __speak_quote(self, i, s, outfmt=None):
+    def __speak_quote(self, i, qt, outfmt=None):
         ofname = str(i).zfill(6)
-        sample_rate = 48000
-        speaker='en_0'
-        audio_paths = self.model.save_wav(text=s,
-                            speaker=speaker,
-                            sample_rate=sample_rate)
-        logger.info('i=%d, model.save_wav complete', i)
+        
+        lang = 'eng'
+        if 'l' in qt:
+            lang = qt['l']
+        text = qt['t'] + ', ' + qt['a']
+        self.__speak_text(text, lang, '1.wav')
+
+        if 'txr' in qt:
+            lang = 'eng'
+            text = qt['txr'] + ', ' + qt['a']
+            self.__speak_text(text, lang, '2.wav')
+            with open('alist', 'w') as f:
+                f.write("file '1.wav'\nfile '2.wav'")
+            os.system("ffmpeg -f concat -i alist -c copy out.wav")
+            os.remove('1.wav')
+            os.remove('2.wav')
+            os.remove('alist')
+        else:
+            os.rename('1.wav', 'out.wav')
+
         if outfmt == 'wav':
-            os.rename('test.wav', f'{ofname}.wav')
+            os.rename('out.wav', f'{ofname}.wav')
         elif outfmt == 'mp3':
-            cmd = f"ffmpeg -i test.wav -ar 44100 -ac 2 -ab 192k -f mp3 {ofname}.mp3"
+            cmd = f"ffmpeg -i out.wav -ar 44100 -ac 2 -ab 192k -f mp3 {ofname}.mp3"
             logger.info('i=%d, outfmt=%s, cmd:=%s', i, outfmt, cmd)
             os.system(cmd)
-            os.remove('test.wav')
+            os.remove('out.wav')
         else:
             logger.error('invalid outfmt')
+
+    def __speak_text(self, text, lang, ofname):
+        sample_rate = 48000
+        model = self.models['eng']
+        speaker = self.lang_speakers['eng']        
+        if lang in self.models.keys():
+            model = self.models[lang]
+        if lang in self.lang_speakers:
+            speaker = self.lang_speakers[lang]
+        if self.random_speaker:
+            speaker = 'random'
+        audio_paths = model.save_wav(text=text,
+                            speaker=speaker,
+                            sample_rate=sample_rate)
+        logger.info('model.save_wav complete. audiopaths=%s', repr(audio_paths))
+        os.rename('test.wav', ofname)
 
     def _add_hashtags(self, qt, s):
         if 'tags' in qt:
